14d95de45a914c089ffaa5cafaf988b4f9419f96
FerN027 <coding.fern27@outlook.com>
2025-11-08 18:15:11 +0800

    Ready for training MobileViT-S

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 # Change this for kaggle training
-# DATA_DIR = "data/FF_faces_simplified"
-DATA_DIR = "/kaggle/input/kaggle-ff"
-
+DATA_DIR = "data/FF_faces_simplified"
+# DATA_DIR = "/kaggle/input/kaggle-ff"
 
 
 
 EPOCHS = 150
 
 
-MODEL_NAMES = ['XceptionNet', 'ViT-B-16']
+MODEL_NAMES = ['XceptionNet', 'ViT-B-16', 'MobileViT-S']
 TITLE = "Loss Curves on FaceForensics++"
-
